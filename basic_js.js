05538e07223af3defac27e3f968e96331691adf3
Anshika Bhadauria <anshikabhadauria1807@gmail.com>
2026-01-08 11:52:34 +0530

    Add files via upload

---
+++ basic_js.js
@@ -0,0 +1,21 @@
+// const rawUsers = [
+//   { id: 1, name: "Rahul", password: "fb_password", role: "admin" },
+//   { id: 2, name: "Sanya", password: "123_password", role: "user" },
+//   { id: 3, name: "Amit", password: "secret_password", role: "user" }
+// ];
+// const safeUsers = rawUsers.map(({ password, ...rest }) => rest);
+// const admins = safeUsers.filter(user => user.role === "admin");
+// console.log(safeUsers);
+// console.log(admins);
+const cart = [
+  { item: "Laptop", price: 50000, quantity: 1, inStock: true },
+  { item: "Mouse", price: 1500, quantity: 2, inStock: true },
+  { item: "Keyboard", price: 3000, quantity: 1, inStock: false }
+];
+const readyToShip = cart.every(product => product.inStock);
+console.log(readyToShip ? "Ready to Ship" : "Wait");
+const availableItems = cart.filter(product => product.inStock);
+const totalBill = availableItems.reduce(
+  (total, product) => total + product.price * product.quantity,0);
+console.log("Available Items:", availableItems);
+console.log("Total Bill:", totalBill);
